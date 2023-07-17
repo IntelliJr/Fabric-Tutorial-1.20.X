d81dc9338abdd8e18e3309afc03b4346394224dd
IntelliJr <2519798@gmail.com>
2023-07-17 12:11:37 +0700

    Videos 1, 2
    Added:
    Ruby, Raw Ruby items
    Ruby item group

--- src/main/java/net/intellijr/tutorialmod/TutorialMod.java
+++ src/main/java/net/intellijr/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.intellijr.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.intellijr.tutorialmod.item.ModItemGroups;
+import net.intellijr.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,6 +21,7 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
