a69f5e8ab86dd0ac89ed5ee869321e4d7743d936
IntelliJr <2519798@gmail.com>
2023-07-22 01:19:08 +0700

    Video 6 - Advanced Items - Metal Detector Item

--- src/main/java/net/intellijr/tutorialmod/item/ModItems.java
+++ src/main/java/net/intellijr/tutorialmod/item/ModItems.java
@@ -14,6 +14,9 @@ public class ModItems {
     public static final Item RUBY = registerItem("ruby", new Item(new FabricItemSettings()));
     public static final Item RAW_RUBY = registerItem("raw_ruby", new Item(new FabricItemSettings()));
 
+    public static final Item METAL_DETECTOR = registerItem("metal_detector",
+            new MetalDetectorItem(new FabricItemSettings().maxDamage(64)));
+
 
     public static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.add(RUBY);
